d0defdb7017bb48d90b09586e67c0a48a3de1d2d
AliZl <ali.zolfagharian@gmail.com>
2022-12-15 19:45:41 +0100

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,1 +1,12 @@
-
+ function get_nut_data(){
+    document.getElementById('cat-text').innerText ='Category of the food is: '+'cheese'
+    document.getElementById('nut-table').style.display='block'
+  }
+  var loadFile = function(event) {
+    document.getElementById('btn').style.display='block'
+    var output = document.getElementById('output');
+    output.src = URL.createObjectURL(event.target.files[0]);
+    output.onload = function() {
+      URL.revokeObjectURL(output.src) // free memory
+    }
+  };
